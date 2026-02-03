dc9759894eacfded311e0dd13bc074c6702f9ddc
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-03 15:10:31 +0200

    Finished making the Book Schema to hold the book fields

--- models/Book.js
+++ models/Book.js
@@ -20,7 +20,34 @@ const bookSchema = new mongoose.Schema(
         }, category: {
             type: String,
             required: true,
-            enum: ['literal-fiction', 'science-fiction', 'mystery', 'science-tech', 'business', 'classics']
+            enum: ['literal-fiction', 'science-fiction', 'mystery', 'science-tech', 'business', 'classics', 'History']
+        },
+        publisher: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        year: {
+            type: Number,
+            required: true,
+            min: 1000
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 5
+        },
+        stock: {
+            type: Number,
+            required: true,
+            min: 0
         }
+    },
+    {
+        timestamps: true
     }
 )
+
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
