2046aa2b624c83524f8c7cf6deb66775a6d7209c
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-04 07:50:54 +0200

    updated the database connection configuration file

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const dbConnect = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database connected Successfully ${conn.connection.host}`);
+        console.log(`Databse: ${conn.connection.name}`)
     } catch (error) {
         console.error(`Database failed to connect ${error.message}`);
         process.exit(1);
