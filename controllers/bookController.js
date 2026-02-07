47aaf9a8d1a79d1091e8a029152a30b0555662f2
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-07 21:48:40 +0200

    made the data that i want from the query params fro the user htat will help me with the pagination of the project

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,7 +2,28 @@ const Book = require('../models/Book')
 
 exports.getBooks = async (req, res) => {
     try {
+        const {
+            // Filtering parameters
+            category,
+            author,
+            minPrice,
+            maxPrice,
+            minRating,
+            maxRating,
+            year,
+            minYear,
+            maxYear,
+            inStock,
+            search, // Search in title and author
 
+            // Sorting parameters
+            sort = 'createdAt',
+            order = 'desc',
+
+            // Pagination parameters
+            page = 1,
+            limit = 10
+        } = req.query;
     } catch (error) {
 
     }
