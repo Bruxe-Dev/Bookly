ebb4e98cb0b4cd4d7c24125e0eaeb1f0dcfc1a2e
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-08 21:26:53 +0200

    finished part of the main server file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const errorHandler = require('./middleware/errorHandler')
 const Book = require('./models/Book')
+const bookRoutes = require('./routes/bookRoute')
+const connectDB = require('./config/db')
 const {
     getBooks,
     getBookBtId,
@@ -10,3 +12,17 @@ const {
     deleteBook,
     getStatistics,
 } = require('../controllers/bookController');
+const app = express()
+
+connectDB() //Connection to the database
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+if (process.env.NODE_ENV == 'development') {
+    app.use((req, res, next) => {
+        console.log(`${req.method} ${req.path}`)
+        next();
+    })
+}
