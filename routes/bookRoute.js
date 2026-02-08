b55b38c673e83ec4adfa8441b45cf3389ac18317
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-08 21:06:27 +0200

    added some routes int othe routes file

---
+++ routes/bookRoute.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+
+const {
+    getBooks,
+    getBookBtId,
+    createBook,
+    updateBook,
+    deleteBook,
+    getStatistics,
+} = require('../controllers/bookController');
+
+router.get('/statistics', getStatistics(req, res));
+
+router.route('/')
+    .get(getBooks)
+    .post(createBook)
+
+router.route('/:id')
+    .get(getBookBtId)
+    .put(updateBook)
+    .delete(deleteBook)
