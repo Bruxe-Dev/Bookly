19d25dc7303cce78ac948463ea413ec37f240480
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-09 07:51:57 +0200

    added a joi validation file to validate the users data input

---
+++ validators/book.validator.js
@@ -0,0 +1,21 @@
+const Joi = require('joi')
+
+const createBookSchema = Joi.object({
+    title: Joi.string().trim().min(1).required,
+    author: Joi.string().trim().required().min(1),
+    price: Joi.number().required().min(0),
+    category: Joi.string().valid(
+        'literal-fiction',
+        'science-fiction',
+        'mystery',
+        'science-tech',
+        'business',
+        'classics',
+        'History'
+    ).required(),
+    publisher: Joi.string().trim().required(),
+    year: Joi.number().min(1000).required(),
+    rating: Joi.number().min(0).max(5).required(),
+    stock: Joi.number().min(0).required(),
+    coverImage: Joi.string().trim().optional
+})
